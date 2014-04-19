8e464708b242cf266b12b40bdd66298cfee9dc64
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2014-04-19 21:17:40 +0300

    Component refactor

--- index.js
+++ index.js
@@ -1,178 +1,202 @@
-var mixinClassList = require('react-mixin-classlist')
-var is = require('is-type')
+var classList = require('react-mixin-classlist')
+
+var B = React.DOM.b
+var I = React.DOM.i
+
+var KEY = {
+  UP: 38
+, DOWN: 40
+, ESC: 27
+, ENTER: 13
+, SPACE: 32
+}
+
+var KEYS = [38, 40, 27, 13, 32]
+
+function noop () {}
 
 
 var defaults = {
-  className: 'react-list-select'
-, focusOn: []
-, focusItemOn: ['click']
-, selectItemOn: ['click', 'space', 'enter']
+  items: []
+, selected: []
+, disabled: []
 , multiple: false
+, onChange: noop
 }
 
-function getSelected (items) {
-  var selected = null
-  items.forEach(function (item) { item.selected && (selected = item) })
-  return selected
+
+var api = {
+  select: function (index) {
+    if (~this.state.disabled.indexOf(index)) return
+
+    var multiple = this.props.multiple
+    var selected = multiple ? this.state.selected.concat(index) : [index]
+
+    this.setState({ selected: selected })
+    this.props.onChange(multiple ? selected : index)
+  }
+, deselect: function (index) {
+    var selected = this.state.selected
+    var indexOf = selected.indexOf(index)
+
+    selected.splice(indexOf, 1)
+
+    this.setState({ selected: selected })
+    this.props.onChange(this.props.multiple ? selected : null)
+  }
+, enable: function (index) {
+    var disabled = this.state.disabled
+    var indexOf = disabled.indexOf(index)
+
+    disabled.splice(indexOf, 1)
+
+    this.setState({ disabled: disabled })
+  }
+, disable: function (index) {
+    this.setState({ disabled: this.state.disabled.concat(index) })
+  }
+, focusItem: function (index) {
+    var focused = this.state.focused
+    var disabled = this.state.disabled
+    var last = this.state.items.length - 1
+
+    if (index == 'next') {
+      if (focused == null) {
+        focused = 0
+      }
+      else {
+        // focus first item if reached last item in the list
+        focused = focused == last ? 0 : focused + 1
+
+        // skip disabled items
+        if (disabled.length) {
+          while (~disabled.indexOf(focused)) {
+            focused = focused == last ? 0 : focused + 1
+          }
+        }
+      }
+    }
+    else if (index == 'previous') {
+      if (focused == null) {
+        focused = last
+      }
+      else {
+        // focus last item if reached the top of the list
+        focused = focused == 0 ? last : focused - 1
+
+        // skip disabled items
+        if (disabled.length) {
+          while (~disabled.indexOf(focused)) {
+            focused = focused == 0 ? last : focused - 1
+          }
+        }
+      }
+    }
+    else if (!~disabled.indexOf(index)) {
+      focused = index
+    }
+
+    this.setState({ focused: focused })
+  }
+, clear: function () {
+    this.setState(defaults)
+  }
+}
+
+
+var internals = {
+  onKeyDown: function (event) {
+    var key = event.keyCode
+    var index = this.state.focused
+
+    if (key == KEY.UP) {
+      this.focusItem('previous')
+    }
+    else if (key == KEY.DOWN) {
+      this.focusItem('next')
+    }
+    else if (key == KEY.SPACE || key == KEY.ENTER) {
+      if (!~this.state.selected.indexOf(index)) {
+        this.select(index)
+      }
+      else {
+        this.deselect(index)
+      }
+    }
+
+    // prevent default behavior, in some situations pressing the key
+    // up / down would scroll the browser window
+    if (~KEYS.indexOf(key)) {
+      event.preventDefault()
+    }
+  }
 }
 
 
 module.exports = React.createClass({
-  mixins: [mixinClassList]
-
+  mixins: [classList, internals, api]
 , getDefaultProps: function () {
     return defaults
   }
-
 , getInitialState: function () {
-    return {
-      items: this.props.items
-    , previous: this.props.multiple ? null : getSelected(this.props.items)
-    }
+    return this.props
   }
-
 , componentWillReceiveProps: function (props) {
-    this.setState({
-      items: props.items
-    , previous: props.multiple ? null : getSelected(props.items)
-    })
+    this.setState(props)
   }
-
 , render: function () {
-    var items = this.state.items.map(function (item, index) {
-      var classes = this.setClassIf({
-        'is-disabled': item.disabled
-      , 'is-selected': item.selected
-      , 'is-focused': item.focused
-      })
-
-      var settings = {
-        className: 'react-list-select-item ' + classes
-      , key: index
-      , onClick: this.onClickItem.bind(this, item)
-      , onDoubleClick: this.onClickItem.bind(this, item)
-      , onMouseEnter: this.onMouseEnterItem.bind(this, item)
-      }
-
-      return React.DOM.b(settings, item.content)
-    }, this)
-
+    var items = this.state.items.map(renderItem, this)
 
     var settings = {
-      className: this.classList(this.props.className)
+      tabIndex: 0
     , ref: 'list'
-    , tabIndex: 0
+    , className: this.classList('react-list-select ' + this.props.className || '')
     , onKeyDown: this.onKeyDown
-    , onBlur: this.onBlur
-    , onMouseEnter: this.onMouseEnter
     }
 
-    return React.DOM.b(settings, items)
-  }
+    ;[
+      'onBlur'
+    , 'onFocus'
+    , 'onMouseEnter'
+    , 'onMouseLeave'
+    ].forEach(function (method) {
+      if (this.props[method]) {
+        settings[method] = this.props[method].bind(this)
+      }
+    }, this)
 
-, onMouseEnter: function () {
-    if (~this.props.focusOn.indexOf('mouseenter'))
-      this.refs.list.getDOMNode().focus()
-  }
-
-, onClickItem: function (item, event) {
-    if (item.disabled) return
-
-    var type = event.type == 'click' ? 'click' : 'doubleclick'
-
-    if (~this.props.focusItemOn.indexOf(type))
-      this.focusItem(item)
-
-    if (~this.props.selectItemOn.indexOf(type))
-      this.selectItem(item)
-  }
-
-, onMouseEnterItem: function (item, event) {
-    if (item.disabled) return
-
-    if (~this.props.focusItemOn.indexOf('mouseenter'))
-      this.focusItem(item)
-  }
-
-, onKeyDown: function (event) {
-    var keys = {
-      '38': 'up'
-    , '40': 'down'
-    , '27': 'esc'
-    , '13': 'enter'
-    , '32': 'space'
-    }
-
-    var key = keys[event.keyCode]
-
-    if ('up' == key || 'down' == key)
-      this.move(key)
-
-    if (~this.props.selectItemOn.indexOf(key))
-      this.selectItem(this.state.focusedItem)
-
-    event.preventDefault()
-  }
-
-, onBlur: function (event) {
-    if (~this.props.selectItemOn.indexOf('blur') && this.state.focusedItem)
-      this.selectItem(this.state.focusedItem)
-  }
-
-, move: function (direction) {
-    var items = this.state.items
-    var indexOf = items.indexOf(this.state.focusedItem)
-
-    function getItem () {
-      if (direction == 'up')
-        var index = (!~indexOf || !indexOf ? items.length - 1 : indexOf - 1)
-
-      if (direction == 'down')
-        var index = (!~indexOf || indexOf == items.length - 1 ? 0 : indexOf + 1)
-
-      var item = items[index]
-
-      // write new indexOf
-      indexOf = items.indexOf(item)
-
-      return item.disabled ? getItem() : item
-    }
-
-    this.focusItem(getItem())
-  }
-
-, position: function (x, y) {
-    var node = this.refs.list.getDOMNode()
-    node.style.top = y + 'px'
-    node.style.left = x + 'px'
-  }
-
-, focusItem: function (item) {
-    if (this.state.focusedItem)
-      this.state.focusedItem.focused = false
-    item.focused = true
-    this.setState({ focusedItem: item })
-  }
-
-, selectItem: function (item) {
-    if (this.state.previous)
-      this.state.previous.selected = false
-
-    item.selected = this.props.multiple ? !item.selected : true
-
-    this.setState({ previous: this.props.multiple ? null : item })
-
-    if (is.function(this.props.onSelect)) {
-      function isSelected (item) { return item.selected }
-      this.props.onSelect(this.state.items.filter(isSelected))
-    }
-  }
-
-, clear: function () {
-    var items = this.state.items.map(function (item) {
-      item.selected = false
-      return item
-    })
-    this.setState({ items: items })
+    return B(settings, items)
   }
 })
+
+
+function renderItem (item, index) {
+  var classes = this.setClassIf({
+    'is-disabled': ~this.state.disabled.indexOf(index)
+  , 'is-selected': ~this.state.selected.indexOf(index)
+  , 'is-focused': this.state.focused == index
+  })
+
+  return (
+    B({
+      key: index
+    , ref: '$' + index
+    , className: 'react-list-select--item ' + classes
+
+    , onMouseEnter: function () {
+        if (~this.state.disabled.indexOf(index)) return
+
+        this.setState({ focused: index })
+      }.bind(this)
+
+    , onClick: function (event) {
+        if (~this.state.selected.indexOf(index)) {
+          this.deselect(index)
+        }
+        else {
+          this.select(index)
+        }
+      }.bind(this)
+    }, item)
+  )
+}
