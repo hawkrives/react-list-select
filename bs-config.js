13058e9b1085f80971921ee0c2a306d223c046bc
Hawken Rives <hawkrives@gmail.com>
2015-02-27 14:47:30 -0600

    Move away from gulp

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	// browser: 'google chrome',
+	open: false,
+	files: ['example/*.css', 'example/*.js', 'example/*.html'],
+	server: {
+		baseDir: './example',
+	},
+	notify: false,
+}
