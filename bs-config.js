84963a8555990c1294ed3af67234ec0b7e74ff19
Hawken Rives <hawkrives@gmail.com>
2015-02-27 14:52:03 -0600

    Remove browser-sync

--- bs-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	// browser: 'google chrome',
-	open: false,
-	files: ['example/*.css', 'example/*.js', 'example/*.html'],
-	server: {
-		baseDir: './example',
-	},
-	notify: false,
-}
